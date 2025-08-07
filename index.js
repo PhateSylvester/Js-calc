e84af4f89bf20010ba0292e560fb484c41a65efc
Phatesylvester <Phatevester@yahoo.com>
2025-08-07 16:59:46 +0100

    made some touches

--- index.js
+++ index.js
@@ -28,11 +28,34 @@ function Vidafa() {
     {
         document.getElementById('outcome').value = digit1%digit2; 
     }
-    
-    // if (vidafa === 0) {
-    //     return "You cannot divide a number by ZERO";
-    // }
-    // else {
-    //     return digit1 / digit2;
-    // }
+    // Check if the inputs are valid numbers
+  if (isNaN(digit1) || isNaN(digit2)) {
+    outcome.value = "Please enter valid numbers";
+    return;
+  }
+
+  // Check if the second digit is zero for division and modulus operations
+  if ((vidafa === "divide" || vidafa === "modulus") && digit2 === 0) {
+    outcome.value = "Cannot divide/modulus by 0";
+    return;
+  }
+
+  // switch (vidafa) {
+  //   case "add":
+  //     outcome.value = digit1 + digit2;
+  //     break;
+  //   case "subtract":
+  //     outcome.value = digit1 - digit2;
+  //     break;
+  //   case "multiply":
+  //     outcome.value = digit1 * digit2;
+  //   case "divide":
+  //     outcome.value = digit1 / digit2;
+  //     break;
+  //   case "modulus":
+  //     outcome.value = digit1 % digit2;
+  //     break;
+  //   default:
+  //     outcome.value = "Invalid operation";
+  // }
 }
